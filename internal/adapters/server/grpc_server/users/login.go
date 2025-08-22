60b3d0e86a79b612a9f34e174518f36dda31cf65
sinhnguyen1411 <ng.dms1411@gmail.com>
2025-08-22 16:47:58 +0700

    Update login.go

--- internal/adapters/server/grpc_server/users/login.go
+++ internal/adapters/server/grpc_server/users/login.go
@@ -2,6 +2,7 @@
 
 import (
 	"context"
+
 	"github.com/sinhnguyen1411/stock-trading-be/api/grpc/user"
 	"google.golang.org/grpc/codes"
 )
@@ -11,6 +12,6 @@ func (s *UserService) Login(ctx context.Context, req *user.LoginRequest) (*user.
 	return &user.LoginResponse{
 		Code:    uint32(codes.OK),
 		Message: codes.OK.String(),
-		Data:    &user.LoginResponse_Data{Token: "bandan"},
+		Data:    &user.LoginResponse_Data{Token: "alice123"},
 	}, nil
 }
